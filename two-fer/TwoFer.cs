8c24c4f40a869fe41b0e4ebc650ea298687049b7
Egor Kostan <igorkostan@gmail.com>
2020-01-04 13:02:21 -0800

    Two Fer
     I would look into using a different value for your optional argument instead of passing in null. If you passed in a different value, you would not have to do null checking and could just return one string instance instead of making them separate

--- two-fer/TwoFer.cs
+++ two-fer/TwoFer.cs
@@ -4,16 +4,9 @@ public static class TwoFer
 {
     // In order to get the tests running, first you need to make sure the Speak method 
     // can be called both without any arguments and also by passing one string argument.
-    public static string Speak(string v = null)
+    public static string Speak(string v = "you")
     {
-        if (v == null)
-        {
-            return "One for you, one for me.";
-        }
-        else {
-            string str = string.Format("One for {0}, one for me", v);
-            return str;
-        }
-
+        string str = string.Format("One for {0}, one for me.", v);
+        return str;
     }
 }
