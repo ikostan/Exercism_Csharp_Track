ddacefda3df9c71722a2f147ab9cf4425a12a6a3
Egor Kostan <igorkostan@gmail.com>
2020-01-11 16:44:14 -0800

    Update Leap.cs

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -2,10 +2,17 @@ using System;
 
 public static class Leap
 {
+    /* 
+    Given a year, report if it is a leap year.
+    The tricky thing here is that a leap year in the Gregorian calendar occurs:
+    */
     public static bool IsLeapYear(int year)
     {
+        // on every year that is evenly divisible by 4
         if(year % 4 == 0){
+            // except every year that is evenly divisible by 100
             if(year % 100 == 0){
+                // unless the year is also evenly divisible by 400
                 if(year % 400 ==0){
                     return true;
                 }
