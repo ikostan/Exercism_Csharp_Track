d67f1e90a8d467fe5aa3740aec306f95323ef7ac
Egor Kostan <igorkostan@gmail.com>
2020-01-13 11:55:36 -0800

    Update Leap.cs

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -9,23 +9,8 @@ public static class Leap
     public static bool IsLeapYear(int year)
     {
         // on every year that is evenly divisible by 4
-        if(year % 4 == 0){
-            // except every year that is evenly divisible by 100
-            if(year % 100 == 0){
-                // unless the year is also evenly divisible by 400
-                if(year % 400 ==0){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-                return true;
-            }
-        }
-        else{
-            return false;
-        }
+        // except every year that is evenly divisible by 100
+        // unless the year is also evenly divisible by 400
+        return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
     }
 }
