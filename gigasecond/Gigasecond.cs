33071933ec6a1d3077c143b455d6ef6604f809a7
Egor Kostan <igorkostan@gmail.com>
2020-02-02 14:16:56 -0800

    Update Gigasecond.cs
    add delimiters to a large number for better readability.

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -4,6 +4,6 @@ public static class Gigasecond
 {
     public static DateTime Add(DateTime moment)
     {   
-        return moment.AddSeconds(1000000000);
+        return moment.AddSeconds(1_000_000_000);
     }
 }
